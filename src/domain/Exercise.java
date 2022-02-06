9d28d973e9707db292040944c758ea3f74c093d0
exiperon <67110285+exiperon@users.noreply.github.com>
2022-02-06 23:00:21 +0200

    Update Exercise.java

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,30 @@
 package domain;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    public static String Calculate(String text){
+        MessageDigest messageDigest;
+        byte[] digest = new byte[0];
+
+        try {
+            messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.reset();
+            messageDigest.update(text.getBytes());
+            digest = messageDigest.digest();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        BigInteger bigInt = new BigInteger(1, digest);
+        StringBuilder md5Hex = new StringBuilder(bigInt.toString(16));
+
+        while(md5Hex.length() < 32){
+            md5Hex.insert(0, "0");
+        }
+
+        return md5Hex.toString();
     }
 }
